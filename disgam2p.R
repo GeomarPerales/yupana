65926a2a2b1edd1566fe2bb220a3a24031ac8f97
Geomar Perales Apaico <per3app@gmail.com>
2025-05-24 14:59:35 -0500

    Add files via upload

---
+++ disgam2p.R
@@ -0,0 +1,70 @@
+#' Two-parameter Gamma distribution function for 24-hour maximum precipitation
+#'
+#' Two-parameter Gamma distribution function for 24-hour maximum precipitation for the study
+#' of extreme precipitation events.
+#'
+#' @param x Vector or dataframe of daily maximum precipitation values
+#' @param tr OPTIONAL, Vector of return period values, default parameter is NULL.
+#'  Typical values for TR are 2, 5, 10, 20, 50, 100, 200, 500, 1000 years.
+#'
+#' @export
+#'
+#' @import stats
+#' @importFrom stats na.omit sd qgamma
+#'
+#' @author Geomar Perales Apaico
+#'
+#' @name disgam2p
+
+disgam2p <- function(x, tr = NULL){
+  if (is.data.frame(x)) {
+    if (ncol(x) == 1) {
+      x <- as.numeric(x)
+      stop("Si 'x' es un data.frame, debe tener al menos dos columnas.")
+    }
+    x <- x[,2]
+
+  } else if (is.numeric(x)) {
+    x <- x
+  } else {
+    stop("El argumento 'x' debe ser un data.frame o un vector numerico.")
+  }
+
+  x <- as.numeric(x)
+  x <- na.omit(x)
+
+  if (length(x) == 0) {
+    stop("No hay datos validos para procesar despues de eliminar NA.")
+  }
+
+  x <- x * 1.13
+  xm <- mean(x)
+  s <- sd(x)
+
+  co <- 2.515517
+  c1 <- 0.802853
+  c2 <- 0.010328
+  d1 <- 1.432788
+  d2 <- 0.189269
+  d3 <- 0.001308
+
+  TR <- c(2, 5, 10, 20, 50, 100, 200, 500, 1000)
+  Px <- 1 - 1 / TR
+
+  alpha <- (xm^2) / (s^2)
+  beta <- (s^2) / xm
+
+  # Escalado de los cuantiles (manual)
+  Q_gamma <- qgamma(Px, shape = alpha, scale = beta)
+
+  if (is.null(tr)) {
+    return(Q_gamma)
+  } else {
+    match.tr <- match(tr, TR)
+    if (is.na(match.tr)) {
+      stop("TR not identified")
+    } else {
+      return(Q_gamma[match.tr])
+    }
+  }
+}
